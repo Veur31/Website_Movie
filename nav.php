c361d62133080dfaa2c533ee7aff84096b79fd9e
Veur31 <tfetesio@gmail.com>
2024-12-14 23:32:00 -0800

    Initialize commit

---
+++ nav.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-custom " >
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon nav-link"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <ul class="navbar-nav mx-auto mt-2 mt-lg-0" style="font-size: 25px; gap:50px;color:#ADD8E6;" >
+	<!--<a class="navbar-brand nav-link" style="font-size: 25px; font-weight: bold;"href="index1.php">Home</a>-->
+	  <li class="nav-item">
+        <a class="nav-link active " href="login.php" style="color: #ADD8E6;">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link active" href="register.php" style="color: #ADD8E6;">Register</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link active" href="content_login.php" style="color: #ADD8E6;">About us</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link active" href="contactus_login.php" style="color: #ADD8E6;">Contact Us</a>
+      </li>
+    </ul>
+
+
+
+  </div>
+</nav>
---
+++ nav_memberspage.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-custom " >
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon nav-link"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <ul class="navbar-nav mx-auto mt-2 mt-lg-0" style="font-size: 20px; color:#ADD8E6;">
+    <a class="navbar-brand nav-link" style="font-size: 25px; font-weight: bold; color: #ADD8E6;"href="members_page.php">About us</a>
+    <li class="nav-item">
+        <a class="nav-link active" href="content.php" style="color: #ADD8E6;">Search a movie</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link active" href="changepass.php" title="View  Users" style="color: #ADD8E6;">Change Password</a>
+      </li>
+
+	  <li class="nav-item">
+        <a class="nav-link active" href="contact.php" style="color: #ADD8E6;">Contact Us</a>
+      </li>
+	  
+	  <li class="nav-item">
+        <a class="nav-link active" href="logout.php" style="color: #ADD8E6;">Logout</a>
+      </li>
+	  
+    </ul>
+</nav>
+
